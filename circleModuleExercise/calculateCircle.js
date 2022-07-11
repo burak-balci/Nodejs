7f794014b4ec2b53ac7c7f09d6a397562690c8a1
Burak <96511762+burak-balci@users.noreply.github.com>
2022-07-11 20:41:00 +0300

    calculate circle app is done.

---
+++ circleModuleExercise/calculateCircle.js
@@ -0,0 +1,5 @@
+const circleArea = (r) => Math.PI * r * r;
+
+const circleCircumference = (r) => Math.PI * (r + r);
+
+module.exports = { circleArea, circleCircumference };
---
+++ circleModuleExercise/index.js
@@ -0,0 +1,7 @@
+const { circleArea, circleCircumference } = require("./calculateCircle");
+
+console.log(
+  `Circle Area : ${circleArea(5)}, Circle Circumference : ${circleCircumference(
+    5
+  )}`
+);
