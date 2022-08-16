40fadf1f1bb324c51f60f3cc2d693904033fd6ec
Burak <96511762+burak-balci@users.noreply.github.com>
2022-08-16 16:50:39 +0300

    Revert "5th part is done."
    This reverts commit f9ef4c0ce89739d41f143d852ca6f8d3bcfa9db5.

--- CleanBlog/app.js
+++ CleanBlog/app.js
@@ -1,11 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const methodOverride = require("method-override");
 const ejs = require("ejs");
 const path = require("path");
 const Post = require("./models/Post");
-const postController = require("./controllers/postControllers");
-const pageController = require("./controllers/pageControllers");
 
 const app = express();
 
@@ -18,21 +15,37 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(
-  methodOverride("_method", {
-    methods: ["POST", "GET"],
-  })
-);
 
-app.get("/", postController.getAllPosts);
-app.get("/posts/:id", postController.getPost);
-app.put("/posts/:id", postController.updatePost);
-app.post("/posts", postController.createPost);
-app.delete("/posts/:id", postController.deletePost);
+app.get("/", async (req, res) => {
+  const posts = await Post.find({});
+  res.render("index", {
+    posts,
+  });
+});
 
-app.get("/about", pageController.getAboutPage);
-app.get("/add_post", pageController.getAddPage);
-app.get("/posts/edit/:id", pageController.getEditPage);
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/posts/:id", async (req, res) => {
+  const post = await Post.findById(req.params.id);
+  res.render("post", {
+    post,
+  });
+});
+
+app.get("/post", (req, res) => {
+  res.render("post");
+});
+
+app.get("/add_post", (req, res) => {
+  res.render("add_post");
+});
+
+app.post("/posts", async (req, res) => {
+  await Post.create(req.body);
+  res.redirect("/");
+});
 
 const port = 3000;
 
